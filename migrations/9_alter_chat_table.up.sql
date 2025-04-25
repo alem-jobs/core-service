b8c067f6145afa87fcae68d421d53518fc81e2af
aidosgal <agalimzhan928@gmail.com>
2025-04-25 19:53:26 +0500

    fix(chat): delete the migration

--- migrations/9_alter_chat_table.up.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE messages DROP CONSTRAINT IF EXISTS fk_receiver;
-
-ALTER TABLE messages ADD CONSTRAINT fk_receiver_org 
-    FOREIGN KEY (receiver_organization_id) REFERENCES organizations(id) ON DELETE CASCADE;
